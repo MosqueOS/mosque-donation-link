56e141a295db1f97c8cfdb3da7baaba013ec2925
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-12-23 22:37:43 +0000

    add the ability to have temporary redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    const tempRedirectUrl = process.env.NEXT_PUBLIC_TEMP_REDIRECT_URL ?? false
+    if (tempRedirectUrl) {
+      return [
+        {
+          source: '/',
+          destination: tempRedirectUrl,
+          permanent: false,
+        },
+      ]
+    } else {
+      return []
+    }
+  },
 }
 
 module.exports = nextConfig
